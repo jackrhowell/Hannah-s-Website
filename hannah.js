78f91d53a64964da8792f6b55072063eae674708
jackrhowell <160778758+jackrhowell@users.noreply.github.com>
2024-05-03 19:31:44 -0700

    Site Updates

--- hannah.js
+++ hannah.js
@@ -1,19 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
+// Smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
         });
     });
 });
-window.addEventListener('resize', function() {
-    const galleryItems = document.querySelectorAll('.gallery-item');
-    const windowWidth = window.innerWidth;
-
-    galleryItems.forEach(item => {
-      item.style.width = windowWidth * 0.8 + 'px'; // Adjust the factor (0.8) as needed
-    });
-  });
